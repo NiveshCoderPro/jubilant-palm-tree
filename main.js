5673f058aedd1246927773c3224337e3f015238f
NiveshCoderPro <78587468+NiveshCoderPro@users.noreply.github.com>
2021-09-27 19:20:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400,300);
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+canvas.center();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Posenet is intialiazed");
+
+}
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x + 25;
+        noseY = results[0].pose.nose.y - 15;
+        console.log("nose x  = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+function draw() {
+    image(video,0,0,400,300);
+}
+
+function take_snapshot() {
+    save("clown-filter-sample.jpg")
+}
